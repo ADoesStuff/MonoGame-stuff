d0797630ae1ac964238b29aba5c807603535baa3
ADoesStuff <alexsideras1978@gmail.com>
2026-04-05 16:43:49 +0300

    added a (currently broken) fullscreen/windowed switch system

--- Game.cs
+++ Game.cs
@@ -6,6 +6,7 @@ namespace GameNMSP {
 
 	public class MainGame : Game {
 		GraphicsDeviceManager gdm;
+		bool nospam = true;
 		private Model? cube;
 		private Vector3 position = new Vector3(0.0f,0.0f,0.0f);
 		private float rotationY = 0.0f;
@@ -16,7 +17,7 @@ namespace GameNMSP {
 		public Matrix gameWorldRotation;
 
 		Vector3 camPos = new Vector3(5, 5, 5);
-		Vector3 camRot = new Vector3(0, 0, 0);
+		Vector3 camRot = new Vector3(0, 1, 0);
 
 		float rotSpeed = 1.0f;
 		float rotAng = 0;
@@ -24,8 +25,16 @@ namespace GameNMSP {
 		KeyboardState state = new KeyboardState();
 
 		public MainGame() {
+
+			
 			gdm = new GraphicsDeviceManager(this);
-			gdm.GraphicsProfile = GraphicsProfile.HiDef;
+
+
+			//gdm.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+    		//gdm.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+			
+			
+	
 
 			// Content.RootDirectory = "Content";
 			// cube = Content.Load<Model>("3D-Models/cube");
@@ -65,7 +74,14 @@ namespace GameNMSP {
 		}
         protected override void Initialize()
         {
-            LoadContent();
+			gdm.HardwareModeSwitch = false;
+			gdm.IsFullScreen = false;
+			Window.AllowUserResizing = true;
+			gdm.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+			gdm.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+			gdm.ApplyChanges();
+			Console.WriteLine("Height:"+GraphicsDevice.Adapter.CurrentDisplayMode.Height+" Width:"+GraphicsDevice.Adapter.CurrentDisplayMode.Width+" something idek:"+gdm.PreferredBackBufferHeight);
+			base.Initialize();
         }
 		
 		protected override void Draw(GameTime gt)
@@ -73,7 +89,7 @@ namespace GameNMSP {
 
 			if(cube != null)
 			{
-				GraphicsDevice.Clear(Color.SteelBlue);
+				GraphicsDevice.Clear(Color.BlueViolet);
 				DrawModel(cube);
 			}
 				
@@ -82,24 +98,61 @@ namespace GameNMSP {
 		
 		protected override void Update(GameTime gt) {
 			HandleInput();
+			rotationY += rotSpeed;
 			//UpdateCamera(gt);
 			base.Update(gt);
 		}
 
 		private void HandleInput() {
+			
 			state = Keyboard.GetState();
 			Keys[] pressedKeys = state.GetPressedKeys();
-			if(pressedKeys.Contains(Keys.D))
+			if (pressedKeys.Contains(Keys.W))
+			{
+				position.X += 0.1f;
+			}
+			if (pressedKeys.Contains(Keys.S))
+			{
+				position.X -= 0.1f;
+			}
+			if (pressedKeys.Contains(Keys.D))
+			{
+				position.Z += 0.1f;
+			}
+			if (pressedKeys.Contains(Keys.A))
+			{
+				position.Z -= 0.1f;
+			}
+			if(pressedKeys.Contains(Keys.X))
 			{
 				rotationX += rotSpeed;
 			}
-			if (pressedKeys.Contains(Keys.G))
+			if (pressedKeys.Contains(Keys.Z))
 			{
 				rotationZ += rotSpeed;
 			}
-			if (pressedKeys.Contains(Keys.F))
+			if (pressedKeys.Contains(Keys.C))
 			{
 				rotationY += rotSpeed;
+			}
+			if (pressedKeys.Contains(Keys.F11) & nospam)
+			{
+				if (gdm.IsFullScreen == true)
+				{
+					gdm.IsFullScreen = false;
+					gdm.ApplyChanges();
+					nospam = false;
+				}
+				else if (gdm.IsFullScreen == false)
+				{
+					gdm.IsFullScreen = true;
+					gdm.ApplyChanges();
+					nospam = false;
+				}
+			}
+			if (!pressedKeys.Contains(Keys.F11) & !nospam)
+			{
+				nospam = true;
 			}
 			gameWorldRotation =
 				Matrix.CreateRotationX(MathHelper.ToRadians(rotationX)) *
--- obj/Release/net10.0/Game.AssemblyInfo.cs
+++ obj/Release/net10.0/Game.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57abe8d9b46bd01bb0ac5790fb7d5b6c8e80e556")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
